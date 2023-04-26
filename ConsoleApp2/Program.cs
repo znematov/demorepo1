045dd5fdf3bf4948bfe42d05e8fd5afc89440097
znematov <zubaidullonematov@gmail.com>
2023-04-26 11:14:49 +0500

    init

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿DemoClass demo = new();
+demo.DemoMethod();
+
+Console.WriteLine("Hello, World!");
+
+
+class DemoClass
+{
+    private string? _testField;
+
+    public string Type { get; set; } = string.Empty;
+
+    public string DemoMethod()
+    {
+        return "Demo";
+    }
+}
